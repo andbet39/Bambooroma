6c00c9d2895499a29823e09cb6d9875e699a0497
andbet39 <andrea.terzani@gmail.com>
2018-01-26 18:01:44 +0000

    push from container 01/26/18 18:01:43

---
+++ cache/compiled/files/3046b40f21e878fe90fb28dc12857db3.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/user/config/security.yaml',
+    'modified' => 1516988913,
+    'data' => [
+        'salt' => 'TBds0PjqRcL9Iz'
+    ]
+];
