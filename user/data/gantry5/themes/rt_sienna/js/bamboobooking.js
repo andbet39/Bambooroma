c900ee72247e00730a0ca621fa5113e3eda9f489
coder01 <andrea.terzani@gmail.com>
2018-01-28 14:10:22 +0100

    home complete

--- user/data/gantry5/themes/rt_sienna/js/bamboobooking.js
+++ user/data/gantry5/themes/rt_sienna/js/bamboobooking.js
@@ -1,8 +1,36 @@
 jQuery(function() {
-    jQuery( ".g-simplebooking-datepicker" ).datepicker({
+
+    jQuery("#booking_dfrom").datepicker({
+        dateFormat: "dd/mm/yy",
         prevText: '<i class="fa fa-angle-left" aria-hidden="true">'+'<' + '/i>',
-        nextText: '<i class="fa fa-angle-right" aria-hidden="true">'+'<' + '/i>'
+        nextText: '<i class="fa fa-angle-right" aria-hidden="true">'+'<' + '/i>',
+        minDate: "today",
+        onSelect: function () {
+            var dt2 = jQuery('#booking_dto');
+            var dt1 = jQuery('#booking_dfrom');
+            var startDate = jQuery(this).datepicker('getDate');
+            //add 30 days to selected date
+            startDate.setDate(startDate.getDate() + 30);
+            var minDate = jQuery(this).datepicker('getDate','1d');
+            var d1dt = minDate
+            minDate.setDate(minDate.getDate()+1); 
+
+
+            //minDate of dt2 datepicker = dt1 selected day
+            dt2.datepicker('setDate', minDate);
+            //sets dt2 maxDate to the last day of 30 days window
+            dt2.datepicker('option', 'maxDate', startDate);
+            //first day which can be selected in dt2 is selected date in dt1
+            dt2.datepicker('option', 'minDate', minDate);
+            //same for dt1
+            //$(this).datepicker('option', 'minDate', d1dt);
+        }
     });
+        jQuery('#booking_dto').datepicker({
+          dateFormat: "dd/mm/yy",
+          prevText: '<i class="fa fa-angle-left" aria-hidden="true">'+'<' + '/i>',
+          nextText: '<i class="fa fa-angle-right" aria-hidden="true">'+'<' + '/i>'
+        });
 });
 
 jQuery('[data-simplebooking-id]').each(function(index) {
