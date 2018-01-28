4c8ad6a85df1749b6173f78a6be5785e2cee8e19
coder01 <andrea.terzani@gmail.com>
2018-01-28 17:24:11 +0100

    fix camere

--- cache/compiled/files/ac5077be2aa2429c9a113f60aa1cf19b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://rt_sienna/rt_sienna.yaml',
-    'modified' => 1517042143,
-    'data' => [
-        'enabled' => true
-    ]
-];
